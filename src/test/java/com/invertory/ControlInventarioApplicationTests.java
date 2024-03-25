231ebda4352653af9a9a3fe4af480f190dd773cc
RonalMael <ronal.maihuire@gmail.com>
2024-03-24 21:56:07 -0500

    control de inventario proyecto base  de control de inventario

---
+++ src/test/java/com/invertory/ControlInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invertory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
